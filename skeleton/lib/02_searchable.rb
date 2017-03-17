55de62c036742e298dca5043ce8b89214d95b2cb
Moktar Jama <moktarjama@gmail.com>
2017-03-17 17:26:50 -0400

    I promise, I tried my best

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -1,12 +1,25 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    search = DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+    search.map do |el|
+      self.new(el)
+    end
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
