4ac66c4b69d84a4edf4e0bdcf7c4224656ae261e
Moktar Jama <moktarjama@gmail.com>
2017-05-02 10:59:44 -0400

    continue on phase 3

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -10,23 +10,57 @@ class AssocOptions
   )
 
   def model_class
-
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
+
+  attr_reader :class_name, :primary_key, :foreign_key
+
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      class_name: name.capitalize,
+      primary_key: :id,
+      foreign_key: "#{name.underscore}_id".to_sym
+    }
+
+    if options.empty?
+      options = options.merge(defaults)
+    else
+      options = defaults.merge(options)
+    end
+
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+    @foreign_key = options[:foreign_key]
   end
 end
 
 class HasManyOptions < AssocOptions
+
+  attr_reader :class_name, :primary_key, :foreign_key
+
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      class_name: name.capitalize.singularize,
+      primary_key: :id,
+      foreign_key: "#{self_class_name.underscore}_id".to_sym
+    }
+
+    if options.empty?
+      options = options.merge(defaults)
+    else
+      options = defaults.merge(options)
+    end
+
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+    @foreign_key = options[:foreign_key]
   end
 end
 
